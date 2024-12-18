9b9a082958aa225f810026807527f30733e0937a
suue99 <susan.olofin@gmail.com>
2024-12-18 01:33:18 +0100

    event edits

--- index.php
+++ index.php
@@ -1,36 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-  
+
 <?php include 'nav/header.php'; ?>
-    <body>
- 
 
-    <!-- HERO SECTION -->
-    <section class="hero-section">
-      <div class="hero-container">
-        <div class="hero-text">
-          <h2 class="hero-header">
-            Rev. Elijah <br />
-            Oluranti Akinyemi
-          </h2>
-          <h3 class="hero-date">15 Jun 1956 - 29 Nov 2024</h3>
-        </div>
+<body>
 
-        <div class="hero-image">
-          <img src="images/hero-image.png" alt="image of a man" />
-          <p class="hero-quote">A light that will never fade</p>
-        </div>
 
-        <div class="funeral-details">
-          <h5>Funeral Details</h5>
-          <p class="funeral-date">coming soon</p>
-        </div>
+  <!-- HERO SECTION -->
+  <section class="hero-section">
+    <div class="hero-container">
+      <div class="hero-text">
+        <h2 class="hero-header">
+          Rev. Elijah <br />
+          Oluranti Akinyemi
+        </h2>
+        <h3 class="hero-date"> 15 Jun 1956 - 29 Nov 2024 </h3>
       </div>
-    </section>
 
-   
-  </body>
+      <div class="hero-image">
+        <img src="images/hero-image.png" alt="image of a man" />
+        <p class="hero-quote">A light that will never fade</p>
+      </div>
+
+      <div class="details-container">
+        <h5> Funeral Arrangements </h5>
+
+        <p class="funeral-text">Service of Songs (Port Harcourt) <br>
+          Monday 17, February 2025</p>
+
+        <div class="funeral-text">
+          Funeral Service (Lagos) <br>
+          Tuesday 20, February 2025
+        </div>
+
+        <div class="funeral-text">
+          Private Interment (Port Harcourt) <br>
+          Saturday 22, February 2025
+        </div>
+
+      </div>
+    </div>
+  </section>
 
 
-  <?php include 'nav/footer.php'; ?>
+</body>
+
+
+<?php include 'nav/footer.php'; ?>
+
 </html>
